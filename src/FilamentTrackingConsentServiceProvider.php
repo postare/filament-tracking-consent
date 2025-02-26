f6f300a885f90587d9bfc4b5eb60403ea6bc9db3
Francesco Apruzzese <inerba@gmail.com>
2025-02-26 16:33:51 +0100

    Rimuove il comando FilamentTrackingConsent e ottimizza le importazioni nella pagina di consenso al tracciamento.

--- src/FilamentTrackingConsentServiceProvider.php
+++ src/FilamentTrackingConsentServiceProvider.php
@@ -10,7 +10,6 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
-use Postare\FilamentTrackingConsent\Commands\FilamentTrackingConsentCommand;
 use Postare\FilamentTrackingConsent\Testing\TestsFilamentTrackingConsent;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
